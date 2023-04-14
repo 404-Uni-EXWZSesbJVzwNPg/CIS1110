57a74baf207ab440a9d703cf1160eea948d98857
vlcty <88147999+devVelocity@users.noreply.github.com>
2023-04-14 01:19:51 +0100

    done

--- poll.js
+++ poll.js
@@ -1,3 +1,17 @@
+
+
 $(document).ready(function(){
+    function showPollResults(){
+        console.log("hi")
+        $("#radioContainer").empty()
+        $("#radioContainer").append("<h4 class='pollResult' style='width: 70%'>Yes - 70%</h4>")
+        $("#radioContainer").append("<h4 class='pollResult' style='width: 30%'>No - 30%</h4>")
     
+    }
+    $("#pollYes").click(() => {
+        showPollResults()
+    })
+    $("#pollNo").click(() => {
+        showPollResults()
+    })
 })
