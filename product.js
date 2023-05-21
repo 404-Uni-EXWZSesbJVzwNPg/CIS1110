c1b09d1aa1c90e2586d1e39a429ff3f1c0b51a0d
Kieran <88147999+devVelocity@users.noreply.github.com>
2023-05-21 19:44:03 +0100

    Add files via upload

--- product.js
+++ product.js
@@ -1,6 +1,7 @@
 
+
 $(function(){
-    var promise = $.ajax("")
+    var promise = $.ajax("https://cis1110apicw.computing.edgehill.ac.uk/reviews")
     promise.done((data) => {
          console.log(data)
          for(item in data.slice(0,5)){
@@ -37,45 +38,93 @@ $(function(){
             reviewstars = undefined;
          }
     })
+    var loadState = false;
+    
     $("#loadAllReviews").click(function(){
-        $("#reviews").empty();
-        var promise = $.ajax("https://cis1110apicw.computing.edgehill.ac.uk/reviews")
-        promise.done((data) => {
-            console.log(data)
-            for(item in data){
-                console.log("data")
-                var random = Math.floor(Math.random() * 2)
-                var src = undefined;
-                if(random == 0){
-                    src = "images/reviewicon1.jpg";
-                }else{
-                    src = "images/reviewicon2.jpg";
+        if (loadState == false){
+            $("#reviews").empty();
+            var promise = $.ajax("https://cis1110apicw.computing.edgehill.ac.uk/reviews")
+            promise.done((data) => {
+                console.log(data)
+                for(item in data){
+                    console.log("data")
+                    var random = Math.floor(Math.random() * 2)
+                    var src = undefined;
+                    if(random == 0){
+                        src = "images/reviewicon1.jpg";
+                    }else{
+                        src = "images/reviewicon2.jpg";
+                    }
+    
+                    var reviewstars = undefined;
+                    var rating = parseInt(data[item].rating)
+                    console.log(rating)
+                    if(rating == 0){
+                        reviewstars = "<h5></span><span class='normalStar'>*****</span>"
+                    }
+                    else if(rating == 1){
+                        reviewstars = "<h5><span class='starOrange'>*</span><span class='normalStar'>****</span>"
+                    }else if(rating == 2){
+                        reviewstars = "<h5><span class='starOrange'>**</span><span class='normalStar'>***</span>"
+                    }else if (rating == 3){
+                        reviewstars = "<h5><span class='starOrange'>***</span><span class='normalStar'>**</span>"
+                    }else if (rating == 4){
+                        reviewstars = "<h5><span class='starOrange'>****</span><span class='normalStar'>*</span>"
+                    }else if (rating == 5){
+                        reviewstars = "<h5><span class='starOrange'>*****</span></h5>"
+                    }
+                    
+                    $("#reviews").append("<section class='review'><img src='" + src + "' alt='Reviewer Icon'/><section>" + reviewstars + "<h3>" + data[item].nickname + "</h3></section><p>" + data[item].review + "</p></section>")
+                    $("#reviews").append("<hr>")
+                    src = undefined;
+                    reviewstars = undefined;
                 }
-
-                var reviewstars = undefined;
-                var rating = parseInt(data[item].rating)
-                console.log(rating)
-                if(rating == 0){
-                    reviewstars = "<h5></span><span class='normalStar'>*****</span>"
+                $("#loadAllReviews").text("HIDE ALL REVIEWS")
+                loadState = true;
+                console.log(loadState)
+            })
+        }else{
+            $("#reviews").empty();
+            var promise = $.ajax("https://cis1110apicw.computing.edgehill.ac.uk/reviews")
+            promise.done((data) => {
+                console.log(data)
+                for(item in data.slice(0,5)){
+                   console.log("data")
+                   var random = Math.floor(Math.random() * 2)
+                   var src = undefined;
+                   if(random == 0){
+                       src = "images/reviewicon1.jpg";
+                   }else{
+                       src = "images/reviewicon2.jpg";
+                   }
+       
+                   var reviewstars = undefined;
+                   var rating = parseInt(data[item].rating)
+                   console.log(rating)
+                   if(rating == 0){
+                       reviewstars = "<h5></span><span class='normalStar'>*****</span>"
+                   }
+                   else if(rating == 1){
+                       reviewstars = "<h5><span class='starOrange'>*</span><span class='normalStar'>****</span>"
+                   }else if(rating == 2){
+                       reviewstars = "<h5><span class='starOrange'>**</span><span class='normalStar'>***</span>"
+                   }else if (rating == 3){
+                       reviewstars = "<h5><span class='starOrange'>***</span><span class='normalStar'>**</span>"
+                   }else if (rating == 4){
+                       reviewstars = "<h5><span class='starOrange'>****</span><span class='normalStar'>*</span>"
+                   }else if (rating == 5){
+                       reviewstars = "<h5><span class='starOrange'>*****</span></h5>"
+                   }
+                   
+                   $("#reviews").append("<section class='review'><img src='" + src + "' alt='Reviewer Icon'/><section>" + reviewstars + "<h3>" + data[item].nickname + "</h3></section><p>" + data[item].review + "</p></section>")
+                   $("#reviews").append("<hr>")
+                   src = undefined;
+                   reviewstars = undefined;
                 }
-                else if(rating == 1){
-                    reviewstars = "<h5><span class='starOrange'>*</span><span class='normalStar'>****</span>"
-                }else if(rating == 2){
-                    reviewstars = "<h5><span class='starOrange'>**</span><span class='normalStar'>***</span>"
-                }else if (rating == 3){
-                    reviewstars = "<h5><span class='starOrange'>***</span><span class='normalStar'>**</span>"
-                }else if (rating == 4){
-                    reviewstars = "<h5><span class='starOrange'>****</span><span class='normalStar'>*</span>"
-                }else if (rating == 5){
-                    reviewstars = "<h5><span class='starOrange'>*****</span></h5>"
-                }
-                
-                $("#reviews").append("<section class='review'><img src='" + src + "' alt='Reviewer Icon'/><section>" + reviewstars + "<h3>" + data[item].nickname + "</h3></section><p>" + data[item].review + "</p></section>")
-                $("#reviews").append("<hr>")
-                src = undefined;
-                reviewstars = undefined;
-                $("#loadAllReviews").remove()
-            }
-        })
+                $("#loadAllReviews").text("READ ALL REVIEWS")
+                loadState = false;
+                console.log(loadState)
+           })
+        }
     })
 })
